e02c7c77badda3b55b1cf829bdd76873c225aec0
Cristian Florian <cflorian@sdl.com>
2022-04-04 09:42:42 +0300

    Dynamically load categories from OOS.

---
+++ AppStoreIntegrationService/AppStoreIntegrationService/Model/CategoriesResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppStoreIntegrationService.Model
+{
+    public class CategoriesResponse
+    {
+        public List<CategoryDetails> Value { get; set; }
+    }
+}
