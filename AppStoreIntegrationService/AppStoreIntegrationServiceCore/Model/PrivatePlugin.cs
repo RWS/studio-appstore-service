82d1527a517d677bf34de87cb0ee2fd433460851
Catalin Tot <catot@sdl.com>
2022-10-06 10:49:52 +0300

    Removed minimum lenght for plugin name field

--- AppStoreIntegrationService/AppStoreIntegrationServiceCore/Model/PrivatePlugin.cs
+++ AppStoreIntegrationService/AppStoreIntegrationServiceCore/Model/PrivatePlugin.cs
@@ -7,7 +7,6 @@ namespace AppStoreIntegrationServiceCore.Model
         public int Id { get; set; }
 
         [Required]
-        [MinLength(5)]
         public string Name { get; set; }
 
         [Required]
