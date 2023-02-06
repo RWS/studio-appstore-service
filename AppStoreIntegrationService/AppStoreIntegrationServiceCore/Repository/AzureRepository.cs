e298890896d07e94bb8b4022b871167825952852
Catalin Tot <catot@sdl.com>
2023-02-06 10:04:11 +0200

    Merged SDLCOM4238-EmailNotifications with SDLCOM4093-AccessibilityFeatures

--- AppStoreIntegrationService/AppStoreIntegrationServiceCore/Repository/AzureRepository.cs
+++ AppStoreIntegrationService/AppStoreIntegrationServiceCore/Repository/AzureRepository.cs
@@ -5,7 +5,6 @@ using Azure.Identity;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using System.Text;
-using Azure;
 
 namespace AppStoreIntegrationServiceCore.Repository
 {
@@ -15,8 +14,6 @@ namespace AppStoreIntegrationServiceCore.Repository
         private readonly BlobServiceClient _blobServiceClient;
         private readonly BlobContainerClient _container;
         private readonly BlobClient _pluginsBlob;
-        private readonly BlobClient _pluginsBlobBackup;
-        private readonly BlobClient _mappingsBlob;
         private readonly BlobClient _settingsBlob;
 
         public AzureRepository(IConfigurationSettings configurationSettings)
@@ -26,116 +23,41 @@ namespace AppStoreIntegrationServiceCore.Repository
             _container = _blobServiceClient.GetBlobContainerClient(_configurationSettings.BlobName);
 
             CreateIfNotExists(_container, _configurationSettings.PluginsFileName);
-            CreateIfNotExists(_container, _configurationSettings.PluginsFileBackUpPath);
-            CreateIfNotExists(_container, _configurationSettings.MappingFileName);
             CreateIfNotExists(_container, _configurationSettings.SettingsFileName);
         }
 
-        public async Task<List<T>> GetPluginsFromContainer()
-        {
-            string containerContent = await _pluginsBlockBlob.DownloadTextAsync(Encoding.UTF8, null, _blobRequestOptions, null);
-
-        public async Task<PluginResponse<T>> ReadFromContainer()
+        public async Task<PluginResponse<PluginDetails>> GetResponse()
         {
             if (_configurationSettings.PluginsFileName == null)
             {
-                return new PluginResponse<T>();
+                return new PluginResponse<PluginDetails>();
             }
 
             var content = await _pluginsBlob.DownloadContentAsync();
-            return JsonConvert.DeserializeObject<PluginResponse<T>>(content.Value.Content.ToString()) ?? new PluginResponse<T>();
-        }
-
-        public async Task<List<ProductDetails>> GetProductsFromContainer()
-        {
-            return (await ReadFromContainer())?.Products;
+            return JsonConvert.DeserializeObject<PluginResponse<PluginDetails>>(content.Value.Content.ToString()) ?? new PluginResponse<PluginDetails>();
         }
 
         public async Task SaveResponse(PluginResponse<PluginDetails> response)
         {
-            var response = await ReadFromContainer();
-            response.Value = plugins;
             var content = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(response)));
             await _pluginsBlob.UploadAsync(content, new BlobUploadOptions());
         }
 
         public async Task<SiteSettings> ReadSettings()
         {
-            var response = await ReadFromContainer();
-            response.Value = plugins;
-            var content = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(response)));
-            await _pluginsBlobBackup.UploadAsync(content, new BlobUploadOptions());
-        }
+            if (_configurationSettings.SettingsFileName == null)
+            {
+                return new SiteSettings();
+            }
 
-        public async Task UpdateProductsFileBlob(List<ProductDetails> products)
-        {
-            var response = await ReadFromContainer();
-            response.Products = products;
-            var content = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(response)));
-            await _pluginsBlob.UploadAsync(content, new BlobUploadOptions());
-        }
-
-        public async Task UpdateParentsFileBlob(List<ParentProduct> products)
-        {
-            var response = await ReadFromContainer();
-            response.ParentProducts = products;
-            var content = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(response)));
-            await _pluginsBlob.UploadAsync(content, new BlobUploadOptions());
-        }
-
-        private CloudStorageAccount GetCloudStorageAccount()
-        {
-            var content = await _mappingsBlob.DownloadContentAsync();
-            return JsonConvert.DeserializeObject<List<NameMapping>>(content.Value.Content.ToString()) ?? new List<NameMapping>();
-        }
-
-        public async Task<List<ParentProduct>> GetParentProductsFromContainer()
-        {
-            return (await ReadFromContainer())?.ParentProducts;
-        }
-
-        public async Task UpdateMappingsFileBlob(List<NameMapping> mappings)
-        {
-            var content = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(mappings)));
-            await _mappingsBlob.UploadAsync(content, new BlobUploadOptions());
-        }
-
-        public async Task<SiteSettings> GetSettingsFromContainer()
-        {
             var content = await _settingsBlob.DownloadContentAsync();
             return JsonConvert.DeserializeObject<SiteSettings>(content.Value.Content.ToString()) ?? new SiteSettings();
         }
 
-        public async Task UpdateSettingsFileBlob(SiteSettings settings)
+        public async Task SaveSettings(SiteSettings settings)
         {
             var content = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(settings)));
             await _settingsBlob.UploadAsync(content, new BlobUploadOptions());
-        }
-
-        public async Task<string> GetAPIVersionFromContainer()
-        {
-            return (await ReadFromContainer())?.APIVersion ?? "1.0.0";
-        }
-
-        private void CreateContainer(CloudStorageAccount cloudStorageAccount)
-        {
-            var blobName = NormalizeBlobName();
-            _cloudBlobContainer = cloudStorageAccount.CreateCloudBlobClient().GetContainerReference(blobName);
-
-        public async Task UpdateCategoriesFileBlob(List<CategoryDetails> categories)
-        {
-            var response = await ReadFromContainer();
-            response.Categories = categories;
-            var content = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(response)));
-            await _pluginsBlob.UploadAsync(content, new BlobUploadOptions());
-        }
-
-        public async Task UpdateAPIVersion(string version)
-        {
-            var response = await ReadFromContainer();
-            response.APIVersion = version;
-            var content = new MemoryStream(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(response)));
-            await _pluginsBlob.UploadAsync(content, new BlobUploadOptions());
         }
 
         private static void CreateIfNotExists(BlobContainerClient container, string fileName)
